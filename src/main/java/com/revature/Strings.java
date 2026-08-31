dca50a87695a26252f022cb7412b0274d8610ba9
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2026-08-31 13:33:49 -0500

    Added Manipulations w/ StringBuilder

--- src/main/java/com/revature/Strings.java
+++ src/main/java/com/revature/Strings.java
@@ -29,6 +29,23 @@ public class Strings {
         System.out.println(name.substring(6));
         System.out.println(name.substring(6,12));
 
+        // String Manipulation with StringBuilder
+        System.out.println();
+        StringBuilder nameBuilder = new StringBuilder(name);
+        System.out.println(nameBuilder);
+        nameBuilder.reverse();
+        System.out.println(nameBuilder);
+        nameBuilder.reverse();
+        nameBuilder.append(" Billerton");
+        System.out.println(nameBuilder);
+        nameBuilder.insert(15, "The Third ");
+        System.out.println(nameBuilder);
+        int startOfExcess = nameBuilder.indexOf("The Third");
+        int endOfExcess = nameBuilder.lastIndexOf(" ");
+        System.out.println(startOfExcess);
+        System.out.println(endOfExcess);
+        nameBuilder.replace(startOfExcess,endOfExcess + 1, "");
+        System.out.println(nameBuilder);
 
     }
 }
