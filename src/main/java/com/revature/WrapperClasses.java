a089dc3067c3ab25b81f3d0773d37e22cfc2aaa6
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2026-08-31 13:53:01 -0500

    Add Helper Classes

--- src/main/java/com/revature/WrapperClasses.java
+++ src/main/java/com/revature/WrapperClasses.java
@@ -1,7 +1,20 @@
 package com.revature;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WrapperClasses {
     public static void main(String[] args) {
 
+        List<Integer> myNumbers = new ArrayList<>();
+        myNumbers.add(1);
+        myNumbers.add(2);
+        myNumbers.add(3);
+
+        System.out.println(myNumbers.getFirst());
+
+        // Helper Classes
+        int myNumber = Integer.parseInt("23");
+        System.out.println(myNumber);
     }
 }
