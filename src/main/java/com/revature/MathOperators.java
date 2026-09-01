b2e255de5019841989531476df09c67551245682
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2026-09-01 13:19:48 -0500

    Adding MathOperators

--- src/main/java/com/revature/MathOperators.java
+++ src/main/java/com/revature/MathOperators.java
@@ -2,6 +2,25 @@ package com.revature;
 
 public class MathOperators {
     public static void main(String[] args) {
+        // Simple Math Operators (+,-,*,/)
+        int add = 5 + 5;
+        int subtract = 5 - 5;
+        int multiplication = 5 * 5;
+        int division = 5 / 5;
 
+        // Variable Arithmetic
+        int num = 5;
+        num = num + 5;
+        num += 5;
+
+        // Incrementation/Decrementation
+        num++;
+        num--;
+
+        // Division Operators (/,%)
+        int divisionOne = 10 / 3;
+        System.out.println(divisionOne);
+        int divisionTwo = 10 % 3;
+        System.out.println(divisionTwo);
     }
 }
